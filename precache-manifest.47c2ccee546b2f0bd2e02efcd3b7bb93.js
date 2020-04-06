11c200f86d4b3004ab8480565d9d70b509824220
Viral Haria <viralhar@gmail.com>
2020-04-06 15:31:23 +0530

    Updates

---
+++ precache-manifest.47c2ccee546b2f0bd2e02efcd3b7bb93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fafcd278eef091808dfde3a3b1d8e85f",
+    "url": "/react-shopper/index.html"
+  },
+  {
+    "revision": "06a9f951664b56650577",
+    "url": "/react-shopper/static/css/main.54b4c1bd.chunk.css"
+  },
+  {
+    "revision": "435b0c5eb124739d21ed",
+    "url": "/react-shopper/static/js/2.9c5524c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-shopper/static/js/2.9c5524c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a9f951664b56650577",
+    "url": "/react-shopper/static/js/main.659bd614.chunk.js"
+  },
+  {
+    "revision": "829a77e6cea4b47a9768",
+    "url": "/react-shopper/static/js/runtime-main.0879680d.js"
+  }
+]);
